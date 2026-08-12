942e16f15e1e533d582a3686b97610946087b5eb
kvn4real <90573257+kvn4real@users.noreply.github.com>
2026-08-12 23:57:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Ne jamais mettre : service_role, clé API IA, mot de passe, secret JWT.
 
 const SUPABASE_URL = "https://khhwtdeakahctyckkrjt.supabase.co";
-const SUPABASE_ANON_KEY = "sb_publishable_f3nbZKfb0sghBWa12q_o9w_tMFR_-Hy";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtoaHd0ZGVha2FoY3R5Y2trcmp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1NDE5NDEsImV4cCI6MjEwMjExNzk0MX0.4JCBIhnyyMv232kBeaGkhXmazWt8bSYsNIgjts0ddLo";
 
 // URL de tes Edge Functions (générée automatiquement par Supabase)
 const FUNCTIONS_URL = `${SUPABASE_URL}/functions/v1`;
